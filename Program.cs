ccd3b44ae24c547eea2cd8afa396b88a80ef9797
amorimfe7 <amorimfe7@gmail.com>
2024-01-18 12:54:54 -0300

    criando Menu

--- Program.cs
+++ Program.cs
@@ -3,7 +3,23 @@
     class Program{
 
         static void Main(string[] args){
-            Console.WriteLine("Hello, World!");
+            Menu();
+        }
+
+        static void Menu(){
+            Console.Clear();
+
+            Console.WriteLine("=========== BEM VINDO AO TEXT EDITOR ===========");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            Console.WriteLine("Qual atividade que você deseja realizar?");
+            Console.WriteLine("");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("[1] - Abrir arquivo");
+            Console.WriteLine("[2] - Criar novo arquivo");
+            Console.WriteLine("[0] - Sair");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("");
         }
     }
 }
